e838662003d33c330cf78b3e6da15f85a89a6584
jsyllu <jessicaly.beau@foxmail.com>
2021-02-17 23:00:58 -0500

    create course table-row and style

---
+++ src/components/course-table/course-row.js
@@ -0,0 +1,123 @@
+import React, {useState} from 'react'
+import {Link} from "react-router-dom";
+
+const CourseRow = ({course, deleteCourse, updateCourse}) => {
+    const [editing, setEditing] = useState(false)
+    const [number, setNumber] = useState(course.number)
+    const [title, setTitle] = useState(course.title)
+    const [owner, setOwner] = useState(course.owner)
+    const [term, setTerm] = useState(course.term)
+    const [lastModified, setLastModified] = useState(course.lastModified)
+
+    const saveCourse = () => {
+        setEditing(false)
+        const newCourse = {
+            ...course,
+            number: number,
+            title: title,
+            owner: owner,
+            term: term,
+            lastModified: lastModified
+        }
+        updateCourse(newCourse)
+    }
+
+    return (
+        <tr>
+            <td className="priority-1">
+                {
+                    !editing &&
+                    <Link to="/editor">
+                        {course.number}
+                    </Link>
+                }
+                {
+                    editing &&
+                    <input className="form-control"
+                           type="text"
+                           onChange={(e) => setNumber(e.target.value)}
+                           value={number} />
+                }
+            </td>
+            <td className="priority-3">
+                {
+                    !editing &&
+                    <Link to="/editor">
+                        <span>{course.title}</span>
+                    </Link>
+                }
+                {
+                    editing &&
+                    <input className="form-control"
+                           type="text"
+                           onChange={(e) => setTitle(e.target.value)}
+                           value={title} />
+                }
+            </td>
+            <td className="priority-2">
+                {
+                    !editing &&
+                    <span>{course.owner}</span>
+                }
+                {
+                    editing &&
+                    <input className="form-control"
+                           type="text"
+                           onChange={(e) => setOwner(e.target.value)}
+                           value={owner} />
+                }
+            </td>
+            <td className="priority-4">
+                {
+                    !editing &&
+                    <span>{course.term}</span>
+                }
+                {
+                    editing &&
+                    <input className="form-control"
+                           type="text"
+                           onChange={(e) => setTerm(e.target.value)}
+                           value={term} />
+                }
+            </td>
+            <td className="priority-5">
+                {
+                    !editing &&
+                    <span>{course.lastModified}</span>
+                }
+                {
+                    editing &&
+                    <input className="form-control"
+                           type="date"
+                           onChange={(e) => setLastModified(e.target.value)}
+                           value={lastModified} />
+                }
+            </td>
+            <td>
+                <button onClick={() => deleteCourse(course)}
+                        className="delete-course-btn">
+                    <i className="fas fa-times fa-2x"></i>
+                </button>
+            </td>
+            <td>
+                {
+                    editing &&
+                    <button onClick={() => saveCourse()}
+                            className="select-course-btn">
+                        <i className="fas fa-check fa-2x"></i>
+                    </button>
+                }
+                {
+                    !editing &&
+                    <button onClick={() => setEditing(true)}
+                            className="select-course-btn">
+                        <i className="fas fa-edit fa-2x"></i>
+                    </button>
+                }
+            </td>
+        </tr>
+    )
+}
+
+
+export default CourseRow
