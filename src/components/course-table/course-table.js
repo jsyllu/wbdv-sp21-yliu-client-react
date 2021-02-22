de9cfceea2709f7476cc2501b194506f8405ae42
jsyllu <jessicaly.beau@foxmail.com>
2021-02-22 00:22:55 -0500

    update course table, add input fields for new courses

--- src/components/course-table/course-table.js
+++ src/components/course-table/course-table.js
@@ -10,14 +10,40 @@ class CourseTable
         super(props);
     }
 
+    state = {
+        newCourseTitle: 'New Course Title',
+        newCourseID: 'CS-0000-00',
+        newCourseOwner: 'Jess Liu',
+        newCourseTerm: '2021 Spring',
+        newCourseDate: '2021-02-22'
+    }
+
     render() {
+
+        const saveNewCourse = () => {
+            const newCourse = {
+                number: this.state.newCourseID,
+                title: this.state.newCourseTitle,
+                owner: this.state.newCourseOwner,
+                term: this.state.newCourseTerm,
+                lastModified: this.state.newCourseDate
+            }
+            // reset title field
+            this.setState({
+                newCourseTitle: 'New Course Title',
+                newCourseID: 'CS-0000-00',
+                newCourseOwner: 'Jess Liu',
+                newCourseTerm: '2021 Spring',
+                newCourseDate: ''
+            })
+            console.log(newCourse)
+            this.props.addCourse(newCourse)
+        }
+
         return (
             <div className="course-table">
-                <h2>Course Table</h2>
                 <div className="btn-group">
-                    <button className="create-course-btn fas fa-plus fa-lg"
-                            onClick={this.props.addCourse}>
-                    </button>
+                    <button className="fas fa-folder"></button>
                     <button className="fas fa-sort-alpha-up fa-lg"></button>
                     <Link to="/courses/grid">
                         <button className="fas fa-th float-right fa-lg"></button>
@@ -35,6 +61,40 @@ class CourseTable
                             <th className="priority-5">Last Modified</th>
                             <th>&nbsp;</th>
                             <th>&nbsp;</th>
+                        </tr>
+                        <tr className="form new-course-form">
+                            <th className="priority-1">
+                                <input type="text"
+                                       className="form-control"
+                                       value={this.state.newCourseID}
+                                       onChange={(e) => this.setState({newCourseID: e.target.value})} />
+                            </th>
+                            <th className="priority-3">
+                                <input type="text" className="form-control"
+                                                              value={this.state.newCourseTitle}
+                                                              onChange={(e) => this.setState({newCourseTitle: e.target.value})} />
+                            </th>
+                            <th className="priority-2">
+                                <input type="text" className="form-control"
+                                                              value={this.state.newCourseOwner}
+                                                              onChange={(e) => this.setState({newCourseOwner: e.target.value})} />
+                            </th>
+                            <th className="priority-4">
+                                <input type="text" className="form-control"
+                                       value={this.state.newCourseTerm}
+                                       onChange={(e) => this.setState({newCourseTerm: e.target.value})} />
+                            </th>
+                            <th className="priority-5">
+                                <input type="date" className="form-control"
+                                       value={this.state.newCourseDate}
+                                       onChange={(e) => this.setState({newCourseDate: e.target.value})} />
+                            </th>
+                            <th>&nbsp;</th>
+                            <th>
+                                <button className="create-course-btn fas fa-plus fa-2x"
+                                        onClick={() => saveNewCourse()}>
+                                </button>
+                            </th>
                         </tr>
                         </thead>
                         <tbody>
