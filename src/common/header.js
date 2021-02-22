6c2b32020fdadce9ef29c384593358a5e59e5698
jsyllu <jessicaly.beau@foxmail.com>
2021-02-22 00:22:10 -0500

    revise header attribute: class to className

--- src/common/header.js
+++ src/common/header.js
@@ -1,62 +1,62 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import './header.css'
 
 const Header = () =>
     // Navbar
-    <nav class="navbar sticky-top navbar-expand-lg navbar-light">
-        <div class="container">
-            {/* Navbar Brand*/}
-            <a class="navbar-brand"
-               href="index.html">
+    <nav className="navbar sticky-top navbar-expand-lg navbar-light">
+        <div className="container">
+            <Link className="navbar-brand"
+                  to="/">
                 Econ<span>Studio</span>
-            </a>
+            </Link>
 
-            {/* collapse without jquery*/}
-            <input type="checkbox" id="no-jq-navbar-toggler" class="d-none" />
+            <input type="checkbox" id="no-jq-navbar-toggler" className="d-none" />
 
-            <label for="no-jq-navbar-toggler"
-                   class="navbar-toggler"
-                   data-toggle="collapse"
-                   data-target="#navbarSupportedContent"
-                   aria-controls="navbarSupportedContent"
-                   aria-expanded="false"
-                   aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <label
+                // for="no-jq-navbar-toggler"
+                className="navbar-toggler"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </label>
 
             <div class="navbar-menu collapse navbar-collapse"
                  id="navbarSupportedContent">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link"
-                           aria-current="page"
-                           href="#">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link className="nav-link"
+                              aria-current="page"
+                              to="/">
                             Home
-                        </a>
+                        </Link>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link"
-                           href="#">
-                            Courses
-                        </a>
+                    <li className="nav-item">
+                        <Link className="nav-link"
+                              to="/">
+                            Gallery
+                        </Link>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link active"
-                           href="/courses/table">
+                    <li className="nav-item">
+                        <Link className="nav-link active"
+                              to="/courses/table">
                             Dashboard
-                        </a>
+                        </Link>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link"
-                           href="#">
+                    <li className="nav-item">
+                        <Link className="nav-link"
+                              to="/">
                             Log In
-                        </a>
+                        </Link>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link"
-                           href="#">
+                    <li className="nav-item">
+                        <Link className="nav-link"
+                              to="/">
                             Register
-                        </a>
+                        </Link>
                     </li>
                 </ul>
             </div>
