4facbd31ed3689387736b24d3995ab2199b23608
jsyllu <jessicaly.beau@foxmail.com>
2021-02-22 00:24:42 -0500

    update course card, put new course card on top

--- src/components/course-grid/course-grid.js
+++ src/components/course-grid/course-grid.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './course-grid.css'
 import CourseCard from "./course-card";
 import {Link} from "react-router-dom";
 
-const CourseGrid = ({courses, addCourse, deleteCourse, updateCourse}) =>
+const CourseGrid = ({courses, addCourse, deleteCourse, updateCourse}) => {
+    const [newCourseTitle, setNewCourseTitle] = useState('New Course Title');
+
+    const saveNewCourse = () => {
+        setNewCourseTitle('New Course Title') // reset title field
+        const createAt = new Date() // today's date
+        const newCourse = {
+            number: 'CS-0000-00',
+            title: newCourseTitle,
+            owner: 'Jess Liu',
+            term: '2021 Spring',
+            lastModified: createAt.toISOString().slice(0,10)
+        }
+        console.log(newCourse)
+        addCourse(newCourse)
+    }
+
+    return (
     <div>
-        <h2>Course Grid {courses.length}</h2>
+        {/*<h2>Course Grid {courses.length}</h2>*/}
         <div className="btn-group">
-            <button className="fas fa-plus fa-lg" onClick={addCourse}></button>
+            {/*<button className="fas fa-plus fa-lg"*/}
+            {/*        onClick={() => saveNewCourse()}></button>*/}
+            <button className="fas fa-folder"></button>
             <button className="fas fa-sort-alpha-up fa-lg"></button>
             <Link to="/courses/table">
                 <button className="fas fa-list fa-lg"></button>
@@ -16,13 +35,6 @@ const CourseGrid = ({courses, addCourse, deleteCourse, updateCourse}) =>
         <hr />
         <div className="curr-enroll">
             <div className="row">
-                {
-                    courses.map(course =>
-                        <CourseCard course={course}
-                                    deleteCourse={deleteCourse}
-                                    updateCourse={updateCourse} />
-                    )
-                }
                 <div className="course-card new-course col-lg-6 col-xl-4">
                     <div>
                         <button style={{color: 'transparent'}}>
@@ -38,7 +50,10 @@ const CourseGrid = ({courses, addCourse, deleteCourse, updateCourse}) =>
                             <div className="card-img"></div>
                             <div className="card-caption">
                                 <figcaption className="card-title">
-                                    New Course Title
+                                    <input className="form-control"
+                                           type="text"
+                                           onChange={(e) => setNewCourseTitle(e.target.value)}
+                                           value={newCourseTitle} />
                                 </figcaption>
                                 <div className="card-detail">
                                     <figcaption>
@@ -47,15 +62,22 @@ const CourseGrid = ({courses, addCourse, deleteCourse, updateCourse}) =>
                                 </div>
                                 <figcaption className="new-icon">
                                     <i className="fa fa-plus fa-2x"
-                                       onClick={() => addCourse()}></i>
+                                       onClick={() => saveNewCourse()}></i>
                                 </figcaption>
                             </div>
                         </figure>
                     </a>
                 </div>
+                {
+                    courses.map(course =>
+                        <CourseCard course={course}
+                                    deleteCourse={deleteCourse}
+                                    updateCourse={updateCourse} />
+                    )
+                }
             </div>
         </div>
     </div>
-
+    )}
 
 export default CourseGrid
