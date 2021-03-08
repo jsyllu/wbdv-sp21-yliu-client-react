91401b7ce1fc039f6e9c4bc851c56de9eb24fcfc
jsyllu <jessicaly.beau@foxmail.com>
2021-03-08 17:22:49 -0500

    create lesson reducer

---
+++ src/reducers/lesson-reducer.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const initialState = {
+    lessons: [
+        // insert lessons data
+    ],
+    foundLesson: {}
+}
+
+const lessonReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "CREATE_LESSON":
+            return {
+                ...state,
+                lessons: [
+                    ...state.lessons,
+                    action.newLesson
+                ]
+            }
+        case "DELETE_LESSON":
+            return {
+                ...state,
+                lessons: state.lessons.filter(lesson => {
+                    return lesson._id !== action.deleteLesson._id;
+                })
+            }
+        case "UPDATE_LESSON":
+            return {
+                ...state,
+                lessons: state.lessons.map(lesson => {
+                    if(lesson._id === action.updateLesson._id) {
+                        return action.updateLesson
+                    } else {
+                        return lesson
+                    }
+                })
+            }
+        case "FIND_LESSONS_FOR_MODULE":
+            return {
+                ...state,
+                lessons: action.lessons
+            }
+        case "FIND_LESSON":
+            return {
+                foundLesson: action.foundLesson
+            }
+        case "CLEAR_CACHED_LESSONS":
+            return {
+                lessons: []
+            }
+        default:
+            return state
+    }
+}
+
+export default lessonReducer
