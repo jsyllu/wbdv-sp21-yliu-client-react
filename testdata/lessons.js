2760ef67301851f15861a63f8a2f68b0a93eda10
jsyllu <jessicaly.beau@foxmail.com>
2021-03-08 17:26:06 -0500

    create sample data for modules, lessons, topics

---
+++ testdata/lessons.js
@@ -0,0 +1,6 @@
+// {title: 'Build', _id:'1'},
+// {title: 'Pages', _id:'2'},
+// {title: 'Theme', _id:'3'},
+// {title: 'Store', _id:'4'},
+// {title: 'Apps', _id:'5'},
+// {title: 'Settings', _id:'6'}
---
+++ testdata/topics.js
@@ -0,0 +1,3 @@
+// {title: 'Topic 1', _id: '1'},
+// {title: 'Topic 2', _id: '2'},
+// {title: 'Topic 3', _id: '3'}
