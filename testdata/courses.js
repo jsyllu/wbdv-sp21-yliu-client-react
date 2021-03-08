fcd5adb8a7c69118d2d61dc6684cf331abfce1a8
jsyllu <jessicaly.beau@foxmail.com>
2021-03-08 17:33:01 -0500

    create sample data for courses

---
+++ testdata/courses.js
@@ -0,0 +1,43 @@
+// {
+//     number: "CS-5610-01",
+//     title: "Web Development",
+//     owner: "Jose Annunziato",
+//     term: "2021 Spring",
+//     lastModified: "2021-02-15",
+//     img: "https://www.sapphirewebsolutions.com/wp-content/uploads/2019/09/Web-Development-Trends.jpg"
+// }, {
+//     number: "CS-5610-02",
+//     title: "Web Development",
+//     owner: "Jose Annunziato",
+//     term: "2021 Spring",
+//     lastModified: "2021-02-10",
+//     img: "https://www.sapphirewebsolutions.com/wp-content/uploads/2019/09/Web-Development-Trends.jpg"
+// }, {
+//     number: "CS-5335-01",
+//     title: "Robotics Science and Systems",
+//     owner: "Robert Platt",
+//     term: "2021 Spring",
+//     lastModified: "2021-02-13",
+//     img: "https://www.clubitc.ro/wp-content/uploads/2019/10/iStock-966248982.jpg"
+// }, {
+//     number: "CS-5010-01",
+//     title: "Program Design Paradigm",
+//     owner: "Clark Freifeld",
+//     term: "2020 Fall",
+//     lastModified: "2020-12-12",
+//     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj4PeHxsZ18nKgK85lGs-qWPpXpBeYvyDKyw&usqp=CAU&ec=45774421"
+// }, {
+//     number: "CS-5800-03",
+//     title: "Algorithms",
+//     owner: "Emanuelle Viola",
+//     term: "2020 Fall",
+//     lastModified: "2020-12-10",
+//     img: "https://www.crn.com/resources/0263-10e24538a932-582b1d7f148d-1000/ai-artificial-intelligence-mind.jpg"
+// }, {
+//     number: "CS-5200-01",
+//     title: "Database Management Systems",
+//     owner: "Martin Schedlbauer",
+//     term: "2020 Summer",
+//     lastModified: "2020-08-15",
+//     img: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c3245c7e-9aa7-406e-8aed-1ecc566959ec/ddhzc2q-795ee219-a11b-44c1-8db4-5ebd5ab09cc3.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvYzMyNDVjN2UtOWFhNy00MDZlLThhZWQtMWVjYzU2Njk1OWVjXC9kZGh6YzJxLTc5NWVlMjE5LWExMWItNDRjMS04ZGI0LTVlYmQ1YWIwOWNjMy5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.JdNPYhHrlrdJNWmoPZVyqp1UWq6SIjGOzEkQnGf9PBU"
+// }
