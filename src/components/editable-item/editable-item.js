5530644a770d6d91fca00251b37bae210b949cb4
jsyllu <jessicaly.beau@foxmail.com>
2021-03-08 23:55:59 -0500

    update editable item to use delete item dialog & floating dropdown

--- src/components/editable-item/editable-item.js
+++ src/components/editable-item/editable-item.js
@@ -1,15 +1,23 @@
 import React, {useState} from "react"
 import {Link} from "react-router-dom"
+import DeleteItemDialog from "../util/delete-item-dialog";
+import DropDownFloating from "../util/drop-down-floating";
 
 const EditableItem = (
     {
         to,
         item,
         updateItem,
-        deleteItem
+        deleteItem,
+        dataType
     }) => {
     const [editing, setEditing] = useState(false)
     const [itemCache, setItemCache] = useState(item)
+    const [deleteDialog, setDeleteDialog] = useState(false)
+    const options = [
+        {key: 'edit', icon: 'fas fa-pencil-alt', text: 'Edit', setFunc: setEditing},
+        {key: 'delete', icon: 'far fa-trash-alt', text: 'Delete', setFunc: setDeleteDialog}
+    ]
     return (
         <>
             {
@@ -18,8 +26,16 @@ const EditableItem = (
                     <Link to={to}>
                         {item.title}&nbsp;
                     </Link>
-                    <i onClick={() => setEditing(true)}
-                       className="fas fa-pencil-alt"></i>
+                    <i>
+                        <DropDownFloating
+                            options={options}
+                            // btnIcon={'fas fa-ellipsis-h'}
+                            dataType={dataType}
+                            btnColor={'lightBlue'}
+                        />
+                    </i>
+                    {/*<i className="fas fa-ellipsis-h"*/}
+                    {/*   onClick={() => setEditing(true)}></i>*/}
                 </>
             }
             {
@@ -29,18 +45,24 @@ const EditableItem = (
                            value={itemCache.title}
                            onChange={(e) =>
                                setItemCache({...itemCache, title: e.target.value})} />
+                    <i onClick={() => setEditing(false)}
+                       className="fas fa-times">&ensp;</i>
                     <i onClick={() => {
                         setEditing(false)
                         updateItem(itemCache)
-                    }} className="fas fa-check"></i>
-                    <i onClick={() => {
-                        setEditing(false)
-                        deleteItem(item)
-                    }} className="fas fa-times"></i>
+                    }} className="fas fa-check">&ensp;</i>
                 </>
+            }
+            {
+                deleteDialog &&
+                <DeleteItemDialog
+                    item={item}
+                    deleteItem={deleteItem}
+                    openDialog={setDeleteDialog}
+                />
             }
         </>
     )
-};
+}
 
 export default EditableItem
