b6bbe1517fc3915801f04ebd9e80f058f5c572a8
jsyllu <jessicaly.beau@foxmail.com>
2021-03-23 16:46:18 -0400

    update version 0.5 in footer

--- src/common/footer.js
+++ src/common/footer.js
@@ -13,7 +13,7 @@ const Footer = () =>
             </a>
         </span>
             <span class="text-muted">
-            &copy; 2021 EconStudio v0.4
+            &copy; 2021 EconStudio v0.5
         </span>
         </div>
     </footer>
