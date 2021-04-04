806ae668e4714942cd94cb7f3b0e36827bafef7b
jsyllu <32682105+jsyllu@users.noreply.github.com>
2021-04-03 22:01:55 -0400

    remove starter files

--- src/index.js
+++ src/index.js
@@ -4,11 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <App/>,
   document.getElementById('root')
 );
-
-reportWebVitals();
