99c5a529786788e0ba20c2129fdcfd805b518dcf
jsyllu <32682105+jsyllu@users.noreply.github.com>
2021-04-15 23:28:46 -0400

    update quiz service - submit quiz

--- src/services/quiz-service.js
+++ src/services/quiz-service.js
@@ -1,4 +1,5 @@
-const NODE_SERVER_URL = 'http://localhost:3001/api'
+// const NODE_SERVER_URL = 'http://localhost:3001/api'
+const NODE_SERVER_URL = 'https://econ-studio-server.herokuapp.com/api'
 
 export const findAllQuizzes = () =>
     fetch(`${NODE_SERVER_URL}/quizzes`)
@@ -8,12 +9,25 @@ export const findQuizzesForCourse = (cid) =>
     fetch(`${NODE_SERVER_URL}/courses/${cid}/quizzes`)
         .then(res => res.json())
 
-export const findQuizzesById = (qid) =>
+export const findQuizById = (qid) =>
     fetch(`${NODE_SERVER_URL}/quizzes/${qid}`)
         .then(res => res.json())
 
+export const submitQuiz = (quizId, questions) =>
+    fetch(`${NODE_SERVER_URL}/quizzes/${quizId}/attempts`, {
+        method: 'POST',
+        body: JSON.stringify(questions),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }).then(res => res.json())
+
+export const findAttemptsForQuiz = (quizId) =>
+    fetch(`${NODE_SERVER_URL}/quizzes/${quizId}/attempts`)
+        .then(res => res.json())
+
 const quiz_api = {
-    findAllQuizzes, findQuizzesForCourse, findQuizzesById
+    findAllQuizzes, findQuizzesForCourse, findQuizById, submitQuiz, findAttemptsForQuiz
 }
 
 export default quiz_api
