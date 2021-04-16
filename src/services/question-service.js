5c84b013c3a297f0168053b69fb2b906a13ef034
jsyllu <32682105+jsyllu@users.noreply.github.com>
2021-04-15 23:29:12 -0400

    update question service

--- src/services/question-service.js
+++ src/services/question-service.js
@@ -1,4 +1,5 @@
-const NODE_SERVER_URL = 'http://localhost:3001/api'
+// const NODE_SERVER_URL = 'http://localhost:3001/api'
+const NODE_SERVER_URL = 'https://econ-studio-server.herokuapp.com/api'
 
 export const findAllQuestions = () =>
     fetch(`${NODE_SERVER_URL}/questions`)
@@ -8,8 +9,8 @@ export const findQuestionsForQuiz = (qid) =>
     fetch(`${NODE_SERVER_URL}/quizzes/${qid}/questions`)
         .then(res => res.json())
 
-const quiz_api = {
+const question_api = {
     findAllQuestions, findQuestionsForQuiz
 }
 
-export default quiz_api
+export default question_api
