cc92d77ebf099c52561ad70316537f1885fdd197
jsyllu <32682105+jsyllu@users.noreply.github.com>
2021-04-15 23:30:35 -0400

    update quizzes list

--- src/components/quizzes/quizzes-list.js
+++ src/components/quizzes/quizzes-list.js
@@ -1,43 +1,45 @@
 import React, {useState, useEffect} from 'react'
 import {Link, useHistory, useParams} from 'react-router-dom'
-import quiz_api from '../../services/quiz-service'
 import course_api from '../../services/course-service'
 import './quizzes-list.style.client.css'
 import Helmet from 'react-helmet'
+import {connect} from 'react-redux'
+import quizActions from '../actions/quiz-actions'
 
-const QuizzesList = () => {
+const QuizzesList = (
+    {
+        quizzes,
+        findQuizzesForCourse,
+        findAllQuizzes
+    }) => {
+
     const {courseId} = useParams()
     const history = useHistory()
     const [course, setCourse] = useState({})
-    const [quizzes, setQuizzes] = useState([])
 
     useEffect(() => {
         course_api.findCourseById(courseId)
             .then((course) => setCourse(course))
-        quiz_api.findAllQuizzes()
-            .then((quizzes) => {
-                setQuizzes(quizzes)
-            })
-        // quiz_api.findQuizzesForCourse(courseId)
-        //     .then(quizzes => setQuizzes(quizzes))
+        findAllQuizzes()
+        // findQuizzesForCourse()
     }, [courseId])
 
     return (
         <>
             <Helmet>
-                <title>Quiz
-                    {/*for {course.title}*/}
-                    | EconStudio</title>
+                <title>
+                    Quizzes {course._id === courseId ? 'for ' + course.title : ''} | EconStudio
+                </title>
             </Helmet>
             <div className="quizzes-list container">
                 <div className="quizzes-header">
-                    <button onClick={() => {
-                        history.goBack()
-                    }}
+                    <button onClick={() => history.goBack()}
                             className="float-left">
                         <i className="fas fa-chevron-left fa-2x"></i>
                     </button>
-                    <h2>Quizzes for {course.number} {course.title}</h2>
+                    <h2>
+                        Quizzes for {course.number} {course.title}
+                    </h2>
                 </div>
                 <div className="quizzes">
                     {
@@ -47,11 +49,12 @@ const QuizzesList = () => {
                                     <div className="quiz"
                                          key={q._id}>
                                         <div className="row">
-                                            <h4>{q.title}</h4>
-
+                                            <h4>
+                                                {q.title}
+                                            </h4>
                                             <Link to={`/courses/${courseId}/quizzes/${q._id}`}>
                                                 <button className="btn btn-outline-primary">
-                                                    Start
+                                                    View
                                                 </button>
                                             </Link>
                                         </div>
@@ -67,4 +70,15 @@ const QuizzesList = () => {
     )
 }
 
-export default QuizzesList
+const stpm = (state) => ({
+    quizzes: state.quizReducer.quizzes
+})
+
+const dtpm = (dispatch) => ({
+    findQuizzesForCourse: (cid) => quizActions.findQuizzesForCourse(dispatch, cid),
+    findAllQuizzes: () => quizActions.findAllQuizzes(dispatch)
+})
+
+export default connect
+(stpm, dtpm)
+(QuizzesList)
