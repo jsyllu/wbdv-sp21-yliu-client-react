d0f509c3da533a5e5b084e4c15f975c3400d7599
jsyllu <32682105+jsyllu@users.noreply.github.com>
2021-04-15 23:31:01 -0400

    update questions list

--- src/components/quizzes/questions/questions-list.js
+++ src/components/quizzes/questions/questions-list.js
@@ -1,58 +1,45 @@
-import React, {useState, useEffect} from 'react'
-import {useHistory, useParams} from 'react-router-dom'
-import './questions-list.style.client.css'
-import question_api from '../../../services/question-service'
-import quiz_api from '../../../services/quiz-service'
+import React from 'react'
 import Helmet from 'react-helmet'
+import './questions-list.style.client.css'
 import QuestionCard from './question-card'
 
-const QuestionsList = () => {
-    const {quizId} = useParams()
-    const history = useHistory()
-    const [quiz, setQuiz] = useState({})
-    const [questions, setQuestions] = useState([])
-
-    useEffect(() => {
-        quiz_api.findQuizzesById(quizId)
-            .then((quiz) => {
-                setQuiz(quiz)
-            })
-        question_api.findQuestionsForQuiz(quizId)
-            .then((questions) => {
-                setQuestions(questions)
-            })
-    }, [quizId])
+const QuestionsList = (
+    {
+        quiz = {},
+        questions = [],
+        viewMode,
+        attemptScore,
+        attemptDate,
+        updateAnswer
+    }) => {
 
     return (
         <>
             <Helmet>
-                <title>Questions
-                    {/*for {quiz.title}*/}
-                    | EconStudio</title>
+                <title>Questions for {quiz.title} | EconStudio</title>
             </Helmet>
 
-            <div className="questions-list container">
-                <div className="quizzes-header">
-                    <button onClick={() => {
-                        history.goBack()
-                    }}
-                            className="float-left">
-                        <i className="fas fa-chevron-left fa-2x"></i>
-                    </button>
-                    <h2>{quiz.title}</h2>
+            {
+                viewMode &&
+                <div className="quiz-attempt-header">
+                    <h4>Score: {Number(attemptScore).toFixed(2)}%</h4>
+                    <h5>Attempted at {attemptDate}</h5>
+                    <hr />
                 </div>
+            }
 
-                <div className="question-card-group">
-                    {
-                        questions.map((q) => {
-                            return (
-                                <QuestionCard question={q}
-                                              key={q._id} />
-                            )
-
-                        })
-                    }
-                </div>
+            <div className="question-card-group">
+                {
+                    questions.map((q, index) => {
+                        return (
+                            <QuestionCard viewMode={viewMode}
+                                          question={q}
+                                          index={index}
+                                          updateAnswer={updateAnswer}
+                                          key={q._id} />
+                        )
+                    })
+                }
             </div>
         </>
     )
